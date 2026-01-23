da7ec8b289c7f1b78ff1fed0067e0c2484931bd8
Koaden <gaelquem44@gmail.com>
2026-01-23 09:26:52 +0100

    fix: phpstan error found

--- apps/back/public/index.php
+++ apps/back/public/index.php
@@ -7,7 +7,6 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context): Kernel {
     /** @var string $env */
     $env = $context['APP_ENV'] ?? 'dev';
-    /** @var bool $env */
     $debug = (bool) ($context['APP_DEBUG'] ?? false);
 
     return new Kernel($env, $debug);
