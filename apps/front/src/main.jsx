88f8238ac9ccd68963411653b6c03fe41f54ecc8
Gael <gael.quemere@ik.me>
2026-02-22 22:16:07 +0100

    feat: adding tailwind css and hero ui

--- apps/front/src/main.jsx
+++ apps/front/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/styles/index.css'
+import {HeroUIProvider} from "@heroui/react"
+import './assets/styles/main.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
